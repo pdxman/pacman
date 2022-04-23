37cf35f55ba5896356d94119e5befc9b5a5252fc
Crawford Vining <crawpdx@gmail.com>
2022-04-23 11:52:17 -0700

    deleted commented code from game file

--- src/Game.js
+++ src/Game.js
@@ -5,15 +5,6 @@ import { ImPacman } from 'react-icons/im';
 export default function Game(props){
     const [pause, setPause] = useState(false)
 
-    // use props(?) to pass different interactive features 
-    // Featues- 
-    // - speed
-    // - direction 
-    // - easter egg- like text scrolling
-    // figure out different buttons/ different functions
-    // update color of ghosts - DONE
-    
-    
     return(
         <>
             <div className={props.divName}>
